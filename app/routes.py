b429588c21bbbb904b9fe92921a7601532a255ce
taehwanNoh <thnoh93@naver.com>
2020-08-19 17:58:08 +0900

    구구단 출력 페이지 추가

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , redirect , request, url_for
  
 app = Flask(__name__)      
  
@@ -10,5 +10,19 @@ def home():
 def about():
   return render_template('about.html')  
  
+@app.route('/calculate')
+@app.route('/calculate/<int:num>')
+def calculate(num=None):
+    return render_template('calculate.html',num=num)
+
+@app.route('/doit',methods=['POST'])
+def doit(num=None):
+    if request.method == 'POST':
+        temp = request.form['num']
+    else:
+        temp = None
+    return redirect(url_for('calculate',num=temp))
+
+
 if __name__ == '__main__':
   app.run(debug=True)
